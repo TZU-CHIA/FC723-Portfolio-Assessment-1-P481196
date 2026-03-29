99b4d02c1b522f81c62552eb180d5c75ccc4ca23
hzijia51-dev <hzijia51@gmail.com>
2026-03-29 20:17:09 +0100

    Second Commit: Refactoring of the code to accept user inputs via the keyboard and addition of explanatory comments.

--- FC723 Portfolio Assessment 1 - Application/Euclidean_Algorithm.py
+++ FC723 Portfolio Assessment 1 - Application/Euclidean_Algorithm.py
@@ -1,21 +1,58 @@
-def find_gcd(A,B):
-    if A < B:
-        A, B = B, A
-    if A == 0:
-        return B
-    if B == 0:
-        return A
-    R = A % B
-    GCD = find_gcd(B, R)
-    return GCD
 
-def check_relative_prime(A, B):
-    GCD = find_gcd(A, B)
-    print(f"The GCD of {A} and {B} is {GCD}.")
-    if GCD == 1:
-        print(f"{A} and {B} are relative prime.")
-    else:
-        print(f"{A} and {B} are not relative prime numbers.")
+class EuclideanAlgorithm:
 
+    def __init__(self):
+        """
+        Initialization function
+        """
+        pass
+
+    def find_gcd(self, A, B):
+        """
+        Function to compute GCD using recursion
+        GCD: Greatest Common Divisor
+        """
+        # Ensure A is greater than B
+        if A < B:
+            A, B = B, A
+
+        # Base case: when remainder is 0
+        if A == 0:
+            return B
+        if B == 0:
+            return A
+
+        # Compute reminder and use recursion compute GCD
+        R = A % B
+        GCD = self.find_gcd(B, R)
+        return GCD
+
+    def check_relative_prime(self):
+        """
+        Function to check relative prime numbers
+        """
+        # Get the two numbers that user want to check if they are relative prime
+        # Try to convert the input strings to integers
+        try:
+            A = int(input("Please enter first number: "))
+            B = int(input("Please enter second number: "))
+        # If they cannot be converted, system will prompt users to rewrite
+        except ValueError():
+            print("Error: Invalid input. Please enter positive integer only.")
+
+        # Get and show GCD
+        GCD = self.find_gcd(A, B)
+        print(f"The GCD of {A} and {B} is {GCD}.")
+
+        # If GCD is 1, they are relative prime
+        if GCD == 1:
+            print(f"{A} and {B} are relative prime.")
+        # If GCD is not 1, they are not relative prime
+        else:
+            print(f"{A} and {B} are not relative prime numbers.")
+
+
+# Instantiate the class
+system = EuclideanAlgorithm()
 # Call the function
-check_relative_prime(88, 99)
+system.check_relative_prime()
